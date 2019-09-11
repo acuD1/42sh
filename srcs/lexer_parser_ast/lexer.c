8d12b5b8412818008555aff5c2ad87ca4502cc86
luckyworld <villat.guillaume@gmail.com>
2019-09-11 12:00:07 +0200

    stash | correction de bugs | OMW

--- srcs/lexer_parser_ast/lexer.c
+++ srcs/lexer_parser_ast/lexer.c
@@ -39,6 +39,7 @@ t_lexer *init_lexer(t_core *shell, char *line)
 	new->buff = line;
 	new->status = START;
 	new->ntok = 0;
+	new->buf_pos = 0;
 	new->szbuff = ft_strlen(line);
 	new->tok = NULL;
 	new->io_here = 0;
@@ -49,17 +50,46 @@ t_lexer *init_lexer(t_core *shell, char *line)
 	return (new);
 }
 
+static void	*token_set(t_token *token, e_tokenid opeid, char *data)
+{
+  if (token->id == TOK_WORD)
+    free(token->data);
+  token->id = opeid;
+  token->data = data;
+  if (data)
+  	token->data_len = ft_strlen(data);
+  else
+  	token->data_len = 0;
+  return (token);
+}
 
 void end_lexer(t_lexer *lexer)
 {
 	// printf("END %u\n", lexer->status);
-	lexer->status = END;
+	if (lexer->buff == '\0')
+		lexer->status = END;
+	else
+	{
+		lexer->status = START;
+		lexer->buf_pos++;
+	}
 }
 
 void name_lexer(t_lexer *lexer)
 {
+	int i;
+
+	i = 0;
+	if (lexer->buff[lexer->buf_pos])
+	{
+		i = lexer->buf_pos;
+		while (lexer->buff[i] != ' ' || lexer->buff[i] != '\t')
+			i++;
+		// ft_add_token(&lexer->tok, TOK_WORD, ft_strsub(lexer->buff, lexer->buf_pos, i));
+		lexer->buf_pos += i + 1;
+	}
 	// ft_add_token(lexer->buff, &lexer->tok);
-	// lexer->ntok++;
+	lexer->ntok++;
 	// printf("NAME %u   %s\n", lexer->status, ((t_token*)lexer->tok->content)->id);
 	// lexer->status = NAME;
 	lexer->status = START;
@@ -117,10 +147,14 @@ void operator_lexer(t_lexer *lexer)
 		{
 			    if (lexer->buf_pos)
 					lexer->buf_pos += ope[i].data_len;
-     			// if (lexer->tok)
-					// ft_add_token(lexer->tok, ope[i].id, ope[i].data);
+     			if (lexer->tok)
+					token_set(lexer->tok->content, ope[i].id, ope[i].data);
+				// printf("%u %s \n", ((t_token*)lexer->tok->content)->id, ((t_token*)lexer->tok->content)->data);
+				printf("%s\n", ope[i].data);
       	}
+      	i++;
 	}
+	lexer->status = START;
 }
 
 void assignement_word_lexer(t_lexer *lexer)
@@ -136,39 +170,30 @@ void assignement_word_lexer(t_lexer *lexer)
 
 void start_lexer(t_lexer *lexer)
 {
-	if (*lexer->buff == '\0')
+	printf("%c\n",lexer->buff[lexer->buf_pos] );
+	if (lexer->buff[lexer->buf_pos] == '\0')
 		lexer->status = END;
-	else if (*lexer->buff == ' ' || *lexer->buff == '\t')
+	else if (lexer->buff[lexer->buf_pos] == ' ' || lexer->buff[lexer->buf_pos] == '\t')
 	{
 		lexer->io_here = 0;
-		while (*lexer->buff == ' ' || *lexer->buff == '\t')
-			lexer->buff++;
+		while (lexer->buff[lexer->buf_pos] == ' ' || lexer->buff[lexer->buf_pos] == '\t')
+			lexer->buf_pos++;
 	}
-	else if (ft_isdigit(*lexer->buff))
-		lexer->status = IO_NUMBER;
-	else if (ft_strchr(OPERATORS, *lexer->buff))
+	// else if (ft_isdigit(lexer->buff[buf_pos]))
+		// lexer->status = IO_NUMBER;
+	else if (ft_strchr(OPERATORS, lexer->buff[lexer->buf_pos]))
+	{
 		lexer->status = OPERATOR;
-	else if (ft_strchr(CHAR_INTERRUPT, *lexer->buff))
-		lexer->status = NEWLINE;
-	else if (ft_strchr("=", *lexer->buff)) // contien un signe = a la fin
-		lexer->status = ASSIGNEMENT_WORD;
+	}
+	// else if (ft_strchr(CHAR_INTERRUPT, lexer->buff[buf_pos]))
+		// lexer->status = NEWLINE;
+	// else if (ft_strchr("=", lexer->buff[buf_pos])) // contien un signe = a la fin
+		// lexer->status = ASSIGNEMENT_WORD;
 	else
 		lexer->status = NAME;
 	printf("statu %u\n", lexer->status);
-}
-
-
-static void	*token_set(t_token *token, e_tokenid opeid, char *data)
-{
-  if (token->id == TOK_WORD)
-    free(token->data);
-  token->id = opeid;
-  token->data = data;
-  if (data)
-  	token->data_len = ft_strlen(data);
-  else
-  	token->data_len = 0;
-  return (token);
+	lexer->buf_pos++;
+	// lexer->status = END;
 }
 
 t_lst	*ft_create_token(char *data, e_tokenid opeid)
@@ -214,27 +239,27 @@ t_lst	*ft_add_token(t_lst **curr, e_tokenid opeid, char *data)
 	return (*curr);
 }
 
-// void ft_printtoklist(t_lexer *lexer)
-// {
-// 	t_token *tmp;
+void ft_printtoklist(t_lexer *lexer)
+{
+	t_token *tmp;
 
-// 	tmp = NULL;
-// 	if (!lexer->tok)
-// 		return;
-// 	printf("list avec %zu node \nAc en data        '%s'  '%u'\n", lexer->ntok, lexer->buff, lexer->status);
-// 	while (lexer->tok->next)
-// 	{
-// 		tmp = (t_token*)lexer->tok->content;
-// 		printf("%s\n", tmp->id);
-// 		lexer->tok = lexer->tok->next;
-// 	}
-// 	while (lexer->tok)
-// 	{
-// 		tmp = (t_token*)lexer->tok->content;
-// 		printf("%s\n", tmp->id);
-// 		lexer->tok = lexer->tok->prev;
-// 	}
-// }
+	tmp = NULL;
+	if (!lexer->tok)
+		return;
+	printf("list avec %zu node \nAc en data        '%s'  '%u'\n", lexer->ntok, lexer->buff, lexer->status);
+	while (lexer->tok->next)
+	{
+		tmp = (t_token*)lexer->tok->content;
+		printf("%s\n", tmp->data);
+		lexer->tok = lexer->tok->next;
+	}
+	while (lexer->tok)
+	{
+		tmp = (t_token*)lexer->tok->content;
+		printf("%s\n", tmp->data);
+		lexer->tok = lexer->tok->prev;
+	}
+}
 
 // void tokenizator(t_lexer *lexer, char *line)
 // {
@@ -268,9 +293,9 @@ void	lexer(t_core *shell, char *line)
 	if (*line == '\0')
 		return ;
 	lexer = init_lexer(shell, line);
-	// while (lexer->status != END)
+	while (lexer->status != END)
 		lexer->lex[lexer->status](lexer);
-	// ft_printtoklist(lexer);
+	ft_printtoklist(lexer);
 
 		// tokenizator(lexer, line); // envoyer line par line a une fct qui va la parcourir et creer une list de token en consequence
 	//parser = parser(lexer, shell);// while (lexer->tok->type != EOF) -> PARSER
