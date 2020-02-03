829d2c8abb28a3d761573d9e5abc3b38155a6948
mpivet-p <mpivet-p@student.42.fr>
2020-02-03 17:13:20 +0100

    linux signals fixed

--- srcs/signals/init_signals.c
+++ srcs/signals/init_signals.c
@@ -6,7 +6,7 @@
 /*   By: mpivet-p <mpivet-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/24 18:59:53 by mpivet-p          #+#    #+#             */
-/*   Updated: 2020/02/03 16:45:48 by mpivet-p         ###   ########.fr       */
+/*   Updated: 2020/02/03 17:13:01 by mpivet-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,57 +58,24 @@ static void	sigh_winch(int signum)
 
 void		init_signals(void)
 {
-
-	signal(SIGHUP, sig_handler);
-	signal(SIGINT, sigint_handler);
-	signal(SIGQUIT, sig_handler);
-	signal(SIGILL, sig_handler);
-	signal(SIGTRAP, sig_handler);
-	signal(SIGABRT, sig_handler);
-	signal(SIGEMT, sig_handler);
-	signal(SIGFPE, sig_handler);
-//kill == null
-	signal(SIGBUS, sig_handler);
-	signal(SIGSEGV, sig_handler);
-	signal(SIGSYS, sig_handler);
-	signal(SIGPIPE, sig_exit);
-	signal(SIGALRM, sig_handler);
-	signal(SIGTERM, sig_handler);
-	signal(SIGURG, sig_handler);
-//STOP = NULL
-	signal(SIGTSTP, sig_handler);
-	signal(SIGCONT, sig_exit);
-	signal(SIGCHLD, SIG_DFL);
-	signal(SIGTTIN, SIG_IGN);
-	signal(SIGTTOU, SIG_IGN);
-	signal(SIGIO, SIG_DFL);
-	signal(SIGXCPU, sig_handler);
-	signal(SIGXFSZ, sig_handler);
-	signal(SIGVTALRM, sig_handler);
-	signal(SIGPROF, sig_handler);
-	signal(SIGWINCH, sigh_winch);
-// INFO null
-	signal(SIGUSR1, sig_handler);
-	signal(SIGUSR2, sig_handler);
-}
-/*
-void		init_signals(void)
-{
-	static void	(*sighandler[32])(int) = {NULL, sig_handler, sigint_handler
+	static int	signals[27] = {SIGHUP, SIGINT, SIGQUIT, SIGILL, SIGTRAP
+			, SIGABRT, SIGFPE, SIGBUS, SIGSEGV, SIGSYS, SIGPIPE, SIGALRM
+			, SIGTERM, SIGURG, SIGTSTP, SIGCONT, SIGCHLD, SIGTTIN, SIGTTOU
+			, SIGIO, SIGXCPU, SIGXFSZ, SIGVTALRM, SIGPROF, SIGWINCH, SIGUSR1
+			, SIGUSR2};
+	static void	(*sighandler[27])(int) = {sig_handler, sigint_handler
 		, sig_handler, sig_handler, sig_handler, sig_handler, sig_handler
-		, sig_handler, NULL, sig_handler, sig_handler, sig_handler, sig_exit
-		, sig_handler, sig_handler, sig_handler, NULL, SIG_IGN, sig_exit
-		, SIG_DFL, SIG_IGN, SIG_IGN, SIG_DFL, sig_handler, sig_handler
-		, sig_handler, sig_handler, sigh_winch, NULL, sig_handler
-		, sig_handler};
+		, sig_handler, sig_handler, sig_handler, sig_exit, sig_handler
+		, sig_handler, sig_handler, sig_handler, sig_exit, SIG_DFL, SIG_IGN
+		, SIG_IGN, SIG_DFL, sig_handler, sig_handler, sig_handler, sig_handler
+		, sigh_winch, sig_handler, sig_handler};
 	int			i;
 
-	i = 1;
-	while (i <= SIGUSR2)
+	i = 0;
+	while (i < 27)
 	{
 		if (sighandler[i] != NULL)
-			if (signal(i, sighandler[i - 1]) != 0)
-				dprintf(STDERR_FILENO, "%i\n", i);
+			signal(signals[i], sighandler[i]);
 		i++;
 	}
-}*/
+}
