9f7e75ed2e53367f75cc710a009e8768bc91b40a
Guillaume VILLAT <guvillat@e1r8p3.42.fr>
2020-02-11 15:48:20 +0100

    re norme

--- srcs/exp/param_exp/error_param_format.c
+++ srcs/exp/param_exp/error_param_format.c
@@ -15,7 +15,8 @@
 char	*error_moar_format_third(char **tablo, char *data)
 {
 	ft_dprintf(STDERR_FILENO,
-		"42sh: %s: %s erreur de syntaxe dans l'expression(le symbole erroné est « %s »)\n", tablo[0], data, data);
+		"42sh: %s: %s erreur de syntaxe dans \
+l'expression(le symbole erroné est « %s »)\n", tablo[0], data, data);
 	ft_tabfree(tablo);
 	return (NULL);
 }
@@ -23,14 +24,16 @@ char	*error_moar_format_third(char **tablo, char *data)
 char	*error_moar_format_bis(char *data)
 {
 	ft_dprintf(STDERR_FILENO,
-		"42sh: %s erreur de syntaxe dans l'expression (le symbole erroné est « : »)\n", data);
+		"42sh: %s erreur de syntaxe dans\
+l'expression (le symbole erroné est « : »)\n", data);
 	return (NULL);
 }
 
 char	*error_moar_format_param(char **tablo, char *data)
 {
 	ft_dprintf(STDERR_FILENO,
-		"42sh: %s erreur de syntaxe dans l'expression (le symbole erroné est « %s »)\n",
+		"42sh: %s erreur de syntaxe dans\
+l'expression (le symbole erroné est « %s »)\n",
 		data, &data[ft_strlen(tablo[0])
 		+ ft_strlen(tablo[1]) + ft_strlen(tablo[2]) + 2]);
 	ft_tabfree(tablo);
