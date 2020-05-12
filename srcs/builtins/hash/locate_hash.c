2a180d2c1a53d2d8db049d0935b74c2eaa857483
acuD1 <arsciand@student.42.fr>
2020-05-12 12:23:52 +0200

    Fixed hash map behavior if binary is moved in PATH

--- srcs/builtins/hash/locate_hash.c
+++ srcs/builtins/hash/locate_hash.c
@@ -26,6 +26,33 @@ static int8_t	del_located_hash(t_core *shell, t_lst *node, t_process *process)
 	return (SUCCESS);
 }
 
+static u_int8_t	check_moved_hash(char *ref, t_lst *node, t_core *shell)
+{
+	t_process	process_tmp;
+	u_int8_t	ret;
+
+	ret = 0;
+	ft_bzero(&process_tmp, sizeof(t_process));
+	if (!(process_tmp.av = malloc(sizeof(char *) * (2))))
+	{
+		hash_error(&shell->hash);
+		return (FALSE);
+	}
+	process_tmp.av[0] = ft_strdup(ref);
+	process_tmp.av[1] = NULL;
+	get_bin_path(shell, &process_tmp);
+	if (process_tmp.bin)
+	{
+		ret = 1;
+		ft_strdel(&((t_db*)node->content)->value);
+		((t_db*)node->content)->value = ft_strdup(process_tmp.bin);
+		((t_db*)node->content)->hit = 1;
+	}
+	ft_strdel(&process_tmp.bin);
+	ft_tabdel(&process_tmp.av);
+	return (ret == 1 ? TRUE : FALSE);
+}
+
 int8_t			locate_hash(t_core *shell, t_process *process)
 {
 	t_lst	*sub_map;
@@ -40,7 +67,10 @@ int8_t			locate_hash(t_core *shell, t_process *process)
 		{
 			if (ft_access(((t_db*)sub_map->content)->value,
 				F_OK | X_OK) != SUCCESS)
-				return (del_located_hash(shell, sub_map, process));
+			{
+				if (check_moved_hash(process->av[0], sub_map, shell) == FALSE)
+					return (del_located_hash(shell, sub_map, process));
+			}
 			process->bin = ft_strdup(((t_db*)sub_map->content)->value);
 			((t_db*)sub_map->content)->hit += 1;
 			return (SUCCESS);
