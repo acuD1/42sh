2817a2b5fe9919d489fddbb6c1933d1019c84845
guvillat <villat.guillaume@gmail.com>
2020-05-13 13:00:35 +0200

    test fanny pour voir

--- srcs/cmdline/auto_c/ac_display.c
+++ srcs/cmdline/auto_c/ac_display.c
@@ -33,16 +33,34 @@ static int8_t	display_confirmed(t_auto_comp *ac)
 	return (SUCCESS);
 }
 
+static void		print_ac_lst(size_t elem_line, , size_t *nb, t_auto_comp *ac)
+{
+	if (elem_line != 1)
+	{
+		if (nb == elem_line)
+		{
+			ft_putchar_fd('\n', STDERR_FILENO);
+			nb = 0;
+		}
+		ft_dprintf(STDERR_FILENO, "%-*s", ac->max_len + 1, ac->lst->content);
+		nb++;
+	}
+	else
+		ft_dprintf(STDERR_FILENO, "%s\n", ac->lst->content);
+}
+
 void			display_ac_lst(t_auto_comp *ac)
 {
 	size_t		elem_line;
 	size_t		nb;
 	t_lst		*head;
 
-	nb = 1;
 	if (!ac->lst)
 		return ;
 	elem_line = ac->ws_col / ac->max_len;
+	if (!elem_line)
+		elem_line = 1;
+	nb = 0;
 	ft_lstsort(&ac->lst, ft_lstcmp);
 	head = ac->lst;
 	ft_putchar_fd('\n', STDERR_FILENO);
@@ -50,14 +68,8 @@ void			display_ac_lst(t_auto_comp *ac)
 		return ;
 	while (ac->lst)
 	{
-		if (elem_line != 0 && nb == elem_line)
-		{
-			ft_putchar_fd('\n', STDERR_FILENO);
-			nb = 1;
-		}
-		ft_dprintf(STDERR_FILENO, "%-*s", ac->max_len + 1, ac->lst->content);
+		print_ac_lst(elem_line, &nb, ac);
 		ac->lst = ac->lst->next;
-		nb++;
 	}
 	ac->lst = head;
 }
